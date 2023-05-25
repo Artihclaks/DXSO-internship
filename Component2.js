afabf46ee6233b6241e3aade76645acae3c0070d
gopi <120123050+gopi7169@users.noreply.github.com>
2023-05-25 13:45:52 +0530

    Add files via upload

---
+++ Component2.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Box from './box.js';
+import App from './App.js';
+function Component2()
+{
+    return(
+        <div>
+            <Box />
+            <App/>
+        </div>
+    );
+
+}
+export default Component2;
